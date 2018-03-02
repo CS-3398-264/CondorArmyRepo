3ab7f89018442819605d92471ec377a14c92c2d1
MichaelMcBride1 <35752461+MichaelMcBride1@users.noreply.github.com>
2018-03-02 17:17:00 -0600

    Add files via upload

---
+++ Chess/Assets/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Chess;
+
+
+namespace Chess
+{
+    public class Game
+    {
+        public static void Main()
+        {
+            GameManager gm = new GameManager();
+
+            gm.playGame();
+        }
+    }
+}
