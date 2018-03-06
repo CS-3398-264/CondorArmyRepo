04689c85753e70ddee63f2f62e537b2448946bb8
Cullen Sturdivant <potatophusion@gmail.com>
2018-03-05 20:27:24 -0600

    Created 2D array structure for the piece positions

--- Chess/Assets/Scripts/GameManager.cs
+++ Chess/Assets/Scripts/GameManager.cs
@@ -3,11 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    
+    // =================
+    // Team one pieces
+    // =================
+    public ChessPiece team1_pawn1;
+    public ChessPiece team1_pawn2;
+    public ChessPiece team1_pawn3;
+    public ChessPiece team1_pawn4;
+    public ChessPiece team1_pawn5;
+    public ChessPiece team1_pawn6;
+    public ChessPiece team1_pawn7;
+    public ChessPiece team1_pawn8;
+
+    public ChessPiece team1_rook1;
+    public ChessPiece team1_rook2;
+
+    public ChessPiece team1_knight1;
+    public ChessPiece team1_knight2;
+
+    public ChessPiece team1_bishop1;
+    public ChessPiece team1_bishop2;
+
+    public ChessPiece team1_queen;
+    public ChessPiece team1_king;
+
+    // =================
+    // Team two pieces
+    // =================
+    public ChessPiece team2_pawn1;
+    public ChessPiece team2_pawn2;
+    public ChessPiece team2_pawn3;
+    public ChessPiece team2_pawn4;
+    public ChessPiece team2_pawn5;
+    public ChessPiece team2_pawn6;
+    public ChessPiece team2_pawn7;
+    public ChessPiece team2_pawn8;
+
+    public ChessPiece team2_rook1;
+    public ChessPiece team2_rook2;
+
+    public ChessPiece team2_knight1;
+    public ChessPiece team2_knight2;
+
+    public ChessPiece team2_bishop1;
+    public ChessPiece team2_bishop2;
+
+    public ChessPiece team2_queen;
+    public ChessPiece team2_king;
+
+    public ChessPiece[,] pieceLocations;
 
     private MouseManager mm;
 
 	// Use this for initialization
 	void Start () {
+        // This is actually disgusting and we should have used a Scriptable Object
+        pieceLocations = new ChessPiece[8, 8] {
+            { team1_rook1, team1_knight1, team1_bishop1, team1_queen, team1_king,  team1_bishop2, team1_knight2, team1_rook2 },
+            { team1_pawn1, team1_pawn2,   team1_pawn3,   team1_pawn4, team1_pawn5, team1_pawn6,   team1_pawn7,   team1_pawn8 },
+            { null,        null,          null,          null,        null,        null,          null,          null        },
+            { null,        null,          null,          null,        null,        null,          null,          null        },
+            { null,        null,          null,          null,        null,        null,          null,          null        },
+            { null,        null,          null,          null,        null,        null,          null,          null        },
+            { team2_pawn8, team2_pawn7,   team2_pawn6,   team2_pawn5, team2_pawn4, team2_pawn3,   team2_pawn2,   team2_pawn1 },
+            { team2_rook2, team2_knight2, team2_bishop2, team2_queen, team2_king,  team2_bishop1, team2_knight1, team2_rook2 }
+        };
+
         mm = FindObjectOfType<MouseManager>();
 	}
 	
--- Chess/Assets/Scripts/Utility Classes/Coordinates.cs
+++ Chess/Assets/Scripts/Utility Classes/Coordinates.cs
@@ -8,6 +8,18 @@ public class Coordinates {
     public int x;
     public int z;
 
+    public Coordinates()
+    {
+        this.x = 0;
+        this.z = 0;
+    }
+
+    public Coordinates(int x, int z)
+    {
+        this.x = x;
+        this.z = z;
+    }
+
     public static Coordinates operator+ (Coordinates a, Coordinates b)
     {
         Coordinates coord = new Coordinates();
