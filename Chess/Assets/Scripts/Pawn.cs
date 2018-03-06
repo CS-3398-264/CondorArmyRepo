17d64b48172d2707c6678cb5ebd4c56eb4e377bf
Unknown <d_r278@txstate.edu>
2018-03-05 19:26:24 -0600

    Minor updates to pawn

--- Chess/Assets/Scripts/Pawn.cs
+++ Chess/Assets/Scripts/Pawn.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Pawn : ChessPiece {
 
     public List<Coordinates> knownMoves;
+    private bool firstMove;
 
     void Start() {
         base.Setup();
+        firstMove = false;
     }
 
     // Update is called once per frame
