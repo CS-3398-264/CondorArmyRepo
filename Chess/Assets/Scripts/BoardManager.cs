4965dab1c44bf47e480d498005250ad71dfd0c86
Cullen Sturdivant <potatophusion@gmail.com>
2018-03-05 18:10:13 -0600

    Added BoardManager and implemented ResetBoard()

---
+++ Chess/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    public Material whiteTile;
+    public Material blackTile;
+
+    private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponentInChildren<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ResetBoard()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            if (i % 2 == 0)
+                rend.material = blackTile;
+            else
+                rend.material = whiteTile;
+        }
+    }
+}
