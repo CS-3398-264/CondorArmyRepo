4450b595be1977b47f7d99cba0090f0f7b3d9fba
Unknown <d_r278@txstate.edu>
2018-03-08 19:58:51 -0600

    PLEASE
    Possibly fixed bug with skynet taking pieces, and improved checkmate

--- Chess/Assets/Scripts/Skynet.cs
+++ Chess/Assets/Scripts/Skynet.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Skynet : MonoBehaviour {
 
     public GameObject parentPiecesObject;
+    public GameManager gm;
     private List<GameObject> pieces;
     private float thinkingTime;
     private bool piecesUpToDate;
@@ -14,27 +15,30 @@ public class Skynet : MonoBehaviour {
 	void Start () {
         pieces = new List<GameObject>();
         UpdatePieces();
+        gm = FindObjectOfType<GameManager>();
 
         thinkingTime = Random.Range(1f, 3f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (GameManager.turn == 1)
+		if (GameManager.turn == 1 && !gm.gameOver.gameObject.activeInHierarchy)
         {
             if (!piecesUpToDate)
             {
                 UpdatePieces();
                 piecesUpToDate = true;
             }
-            List<Coordinates> moves;
+            List<Coordinates> moves = new List<Coordinates>();
             GameObject selectedPiece;
             if (thinkingTime <= 0f) {
                 selectedPiece = pieces[Random.Range(0, pieces.Count)];
+                Debug.Log("sPiece: " + selectedPiece);
                 moves = selectedPiece.GetComponent<ChessPiece>().GetMoves(true);
                 if (moves.Count != 0)
                 {
                     Coordinates move = moves[Random.Range(0, moves.Count)];
+                    Debug.Log("Moving: " + move);
                     selectedPiece.GetComponent<ChessPiece>().move(move);
                     thinkingTime = Random.Range(1f, 3f);
                 }
